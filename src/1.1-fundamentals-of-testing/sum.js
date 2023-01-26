5819fb09b5cdbb96a98d6db40ef46206cbbf7b04
Kateryna Porshnieva <kateryna.porshnieva@veriff.net>
2023-01-26 20:44:05 +0200

    reset example 1.1

--- src/1.1-fundamentals-of-testing/sum.js
+++ src/1.1-fundamentals-of-testing/sum.js
@@ -1,9 +1,14 @@
 const sum = (...numbers) => {
-  return numbers.reduce(
-    (result, number) =>
-      result + (Array.isArray(number) ? sum(...number) : number),
-    0
-  );
+  let result = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+  }
+
+  return result;
 };
 
-module.exports = sum;
+sum(2, 3); // => 5
+sum(2, 3, 4); // => 9
+
+// module.exports = sum;
